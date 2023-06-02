8b5031408c38ea46bf10ccc8de507c2dce0c3217
Dan <danmizu@nostalgi.cc>
2023-06-02 03:56:12 -0700

    7tv api, Twitch API, Emote Usage api, Access Token saving

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,200 @@
 import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import path from "path";
+import fs, { promises as asyncfs } from "fs";
 
-export default class stvAPI {
+// get data
+import config from "../config/config.json" assert { type: "json" };
+
+export default class api {
+	private twitchAccessToken: string;
+	private twitchAccessTokenExpiration: number;
 	readonly userID: string;
-	api: AxiosInstance;
+	username: string;
+
+	twitchAPI: AxiosInstance;
+	stvAPI: AxiosInstance;
+	emoteUsageAPI: AxiosInstance;
 
 	constructor(userID: string) {
 		// save user ID
 		this.userID = userID;
 
-		// create axios instance
-		this.api = axios.create({
+		// create axios instances
+		this.twitchAPI = axios.create({
+			baseURL: "https://api.twitch.tv/helix/",
+		});
+		this.stvAPI = axios.create({
 			baseURL: "https://7tv.io/v3/",
+		});
+		this.emoteUsageAPI = axios.create({
+			baseURL: "https://api.kattah.me/c/",
 		});
 	}
 
-	async get(
-		success: (response: AxiosResponse) => void,
-		fail: (err: AxiosError) => void
-	) {
+	async getUsername() {
+		// username exists
+		if (this.username) return;
+
+		await this.getTwitchData(
+			{ id: this.userID },
+			(response: AxiosResponse) => {
+				this.username = response.data.data[0].display_name;
+			}
+		);
+	}
+
+	async getTwitchAccessToken() {
+		// access token exists and is not expired
+		if (
+			this.twitchAccessToken &&
+			this.twitchAccessTokenExpiration >= Date.now()
+		)
+			return;
+
+		// check temp file
+		if (fs.existsSync(path.join(process.cwd(), "temp.json") as string)) {
+			// get temp data
+			const tempData = await asyncfs.readFile(
+				path.join(process.cwd(), "temp.json"),
+				"utf-8"
+			);
+
+			// get access token data
+			const accessTokenData = JSON.parse(tempData)["access_token"];
+
+			// access token found
+			if (
+				accessTokenData &&
+				accessTokenData.token &&
+				accessTokenData.expires_at
+			) {
+				// save expiration
+				this.twitchAccessTokenExpiration = Number(
+					accessTokenData.expires_at
+				);
+
+				// if not expired, set it
+				if (this.twitchAccessTokenExpiration >= Date.now()) {
+					// save token
+					this.twitchAccessToken = accessTokenData.token;
+
+					// save expiration
+					this.twitchAccessTokenExpiration = Number(
+						accessTokenData.expires_at
+					);
+					return;
+				}
+			}
+		}
+
+		// get new access token
 		try {
-			const response = await this.api.get("users/twitch/" + this.userID);
+			const response: AxiosResponse = await axios.post(
+				"https://id.twitch.tv/oauth2/token",
+				{
+					client_id: config.twitch.client_id,
+					client_secret: config.twitch.client_secret,
+					grant_type: "client_credentials",
+				}
+			);
+
+			// save token
+			this.twitchAccessToken = response.data.access_token;
+
+			// save expiration
+			this.twitchAccessTokenExpiration =
+				Number(response.data.expires_in) + Date.now();
+
+			// save in temp file
+			await asyncfs.writeFile(
+				path.join(process.cwd(), "temp.json"),
+				JSON.stringify({
+					access_token: {
+						token: response.data.access_token,
+						expires_at:
+							Number(response.data.expires_in) + Date.now(),
+					},
+				}),
+				"utf-8"
+			);
+		} catch (err) {
+			console.error(
+				"Failed Access Token Retrieval:",
+				err.response.data.status,
+				err.response.data.message
+			);
+		}
+	}
+
+	async getTwitchData(
+		data: {},
+		success: (response: AxiosResponse) => void,
+		fail?: (err: AxiosError) => void
+	) {
+		// get access token (if doesn't exist or is expired)
+		if (
+			!this.twitchAccessToken ||
+			this.twitchAccessTokenExpiration <= Date.now()
+		)
+			await this.getTwitchAccessToken();
+
+		try {
+			const response: AxiosResponse = await this.twitchAPI.get("users", {
+				params: data,
+				headers: {
+					"Client-Id": config.twitch.client_id,
+					Authorization: "Bearer " + this.twitchAccessToken,
+				},
+			});
 			success(response);
 		} catch (err) {
-			fail(err);
+			if (fail) fail(err);
+			console.error(
+				"Failed Twitch User Retrieval:",
+				err.response.data.status,
+				err.response.data.message
+			);
+		}
+	}
+
+	async get7tvData(
+		success: (response: AxiosResponse) => void,
+		fail?: (err: AxiosError) => void
+	) {
+		try {
+			const response: AxiosResponse = await this.stvAPI.get(
+				"users/twitch/" + this.userID
+			);
+			success(response);
+		} catch (err) {
+			if (fail) fail(err);
+			console.error(
+				"Failed 7TV User Retrieval:",
+				err.response.data.status,
+				err.response.data.message
+			);
+		}
+	}
+
+	async getEmoteUsage(
+		success: (response: AxiosResponse) => void,
+		fail?: (err: AxiosError) => void
+	) {
+		// make sure username is retrieved
+		if (!this.username) await this.getUsername();
+
+		try {
+			const response: AxiosResponse = await this.emoteUsageAPI.get(
+				this.username
+			);
+			success(response);
+		} catch (err) {
+			console.error(
+				"Failed Twitch User Emotes Data Retrieval:",
+				err.response.status,
+				err.response.statusText
+			);
+			if (fail) fail(err);
 		}
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,44 @@
 // imports
 import log from "./utility/log.js";
-import stvAPI from "./api.js";
-import { AxiosError, AxiosResponse } from "axios";
+import api from "./api.js";
+import { AxiosResponse } from "axios";
 import utility from "./utility/utility.js";
 
 // get data
 import config from "../config/config.json" assert { type: "json" };
 
 // init axios
-const sevenTV = new stvAPI(config["7tvUserID"]);
+const twitchAPI = new api(config["7tvUserID"]);
 
-await sevenTV.get(
-	(response: AxiosResponse) => {
-		log.message(
-			`Twitch User ${response.data.display_name} Found: \n` +
-				utility.JSON.stringify(
-					utility.JSON.getObjectsFromKeyValue(
-						response.data.emote_set.emotes,
-						"name",
-						"OMEGALUL"
-					)
+// get emote data
+// await twitchAPI.get7tvData(
+// 	(response: AxiosResponse) => {
+// 		log.message(
+// 			`Twitch User ${response.data.display_name} Found: \n` +
+// 				utility.JSON.stringify(
+// 					utility.JSON.getObjectsFromKeyValue(
+// 						response.data.emote_set.emotes,
+// 						"name",
+// 						config.desiredEmote
+// 					)
+// 				)
+// 		);
+// 	},
+// 	(err: AxiosError) => {
+// 		log.message("Twitch User NOT Found: \n" + err.message);
+// 	}
+// );
+
+// get emote usage data
+await twitchAPI.getEmoteUsage((response: AxiosResponse) => {
+	log.message(
+		`Twitch User ${twitchAPI.username} Found: \n` +
+			utility.JSON.stringify(
+				utility.JSON.getObjectsFromKeyValue(
+					response.data.emotes,
+					"emote",
+					config.desiredEmote
 				)
-		);
-	},
-	(err: AxiosError) => {
-		log.message("Twitch User NOT Found: \n" + err.message);
-	}
-);
+			)
+	);
+});
